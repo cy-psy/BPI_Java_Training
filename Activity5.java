2f1b131cb19a0ee7c59e5126dea3d85a0dda5f21
NII-Cyril A. Emnas <nii-caemnas@headoffice.corp.bpi.com.ph>
2025-12-05 16:42:17 +0800

    M1_ACTIVITY5

---
+++ Activity5.java
@@ -0,0 +1,17 @@
+package Activity5;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c = 1;
+		int sum = 0;
+		
+		while (c <= 50) {
+			sum += c;
+			c++;
+		}
+		System.out.println("Sum = " + sum);
+	}
+
+}
