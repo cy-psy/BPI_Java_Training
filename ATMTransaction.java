49a1071b2cb256d640621d910d0b804de2fe67e6
NII-Cyril A. Emnas <nii-caemnas@headoffice.corp.bpi.com.ph>
2026-01-08 15:18:40 +0800

    M4_ACTIVITY3

--- ATMTransaction.java
+++ ATMTransaction.java
@@ -28,19 +28,25 @@ public class ATMTransaction {
 		} catch (StringIndexOutOfBoundsException e) {
 			System.out.println("Error: Account number is empty or invalid!");
 		} finally {
+			System.out.println();
 			System.out.println("========== RECEIPT ==========");
 			System.out.println("Transaction Date: December 3, 2025");
 			System.out.println("Transaction Type: Balance Inquiry");
 			System.out.println("ATM Location: Main Branch");
 			System.out.println("Thank you for banking with us!");
-			System.out.println("=============================\n");
+			System.out.println("=============================");
+			System.out.println();
 		}
 	}
 
 	public static void main(String[] args) {
+		System.out.println("--- Test Case 1: Valid Savings Account ---");
 		checkBalance("100123456", 15000.00);
+		System.out.println("--- Test Case 2: Valid Checking Account ---");
 		checkBalance("200987654", 25000.00);
+		System.out.println("--- Test Case 3: Invalid Account Number Format ---");
 		checkBalance("ABC12345", 15000.00);
+		System.out.println("--- Test Case 4: Empty Account Number ---");
 		checkBalance("", 15000.00);
 	}
 }
